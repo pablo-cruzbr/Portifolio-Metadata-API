6ba07159dd2b7cac34839c99d127634f53702e07
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2025-11-24 10:00:24 -0300

    feat: Inicio de configuração da inicialização do servidor e rotas iniciais

---
+++ backend/src/server.ts
@@ -0,0 +1,36 @@
+import express, { Request, Response, NextFunction } from "express";
+import 'express-async-errors';
+
+import path from 'path';
+import { router } from "./routes";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT','PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
+
+app.use(router);
+
+
+app.use(((err: Error, _req: Request, res: Response, _next: NextFunction): void => {
+    if (err instanceof Error) {
+        res.status(400).json({
+            error: err.message
+        });
+    } else {
+        res.status(500).json({
+            status: 'error',
+            message: 'Internal Server Error.'
+        });
+    }
+}) as import('express').ErrorRequestHandler);
+
+app.listen(3334, () => {
+    console.log('Servidor SF2 Online na porta 3334!');
+});
