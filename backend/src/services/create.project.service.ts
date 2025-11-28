60c11fa14a1025f39311da93f23ac55db10e92d2
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2025-11-28 16:26:09 -0300

    feat: ajuste de arquivo service unificado com CRUD

--- backend/src/services/create.project.service.ts
+++ backend/src/services/create.project.service.ts
@@ -1,42 +1,84 @@
 import prismaClient from "../prisma/index.js";
 
 interface ProjectRequest{
+    id: string;
     title: string;
     technologies: string;
     goal: string;
     features: string;
 }
 
-class CreateProjectService{
-    async execute(data: ProjectRequest){
-        const { title, technologies, goal, features } = data; 
-
-        if (title === ''){
-            throw new Error('Name Invalid'); 
+interface UpdateProjectRequest{
+    id: string;
+    title: string;
+    technologies: string;
+    goal: string;
+    features: string;
+}
+class ProjectService{
+    async createProject({
+        title,
+        technologies,
+        goal,
+        features
+    }: ProjectRequest){
+        if(!title){
+            throw new Error("É necessário um nome ao projeto !");
         }
-        const produtoExistente = await prismaClient.project.findFirst({
-            where:{
-                title: title
-            }
-        })
-        const project = prismaClient.project.create({
-            data:{
-                 title: title,
-                 technologies: technologies,
-                 goal: goal,
-                 features: features
-            },
 
-            select:{
-                id: true,
-                title: true,
-                technologies: true,
-                goal: true,
-                features: true
-            }
-        })
+        const project = await prismaClient.project.create({
+            data: {
+                title,
+                technologies,
+                goal,
+                features
+            },
+        });
         return project
     }
+
+    async updateProject ({
+        id,
+        title,
+        technologies,
+        goal,
+        features
+    }:UpdateProjectRequest){
+        const project = await prismaClient.project.update({
+            where: {id},
+            data: {
+                title,
+                technologies,
+                goal,
+                features
+            },
+        });
+        return project;
+    }
+
+    async listProjects({ id, title, technologies, goal, features }: ProjectRequest) {
+    const projects = await prismaClient.project.findMany({
+        where: {
+            ...(id && { id }),
+            ...(title && { title: { contains: title, mode: "insensitive" } }),
+            ...(technologies && { technologies: { contains: technologies, mode: "insensitive" } }),
+            ...(goal && { goal: { contains: goal, mode: "insensitive" } }),
+            ...(features && { features: { contains: features, mode: "insensitive" } }),
+        },
+        orderBy: {
+            created_at: "desc"
+        }
+    });
+    return projects;
+    }
+
+    async deleteProject (id: string){
+        await prismaClient.project.delete({
+            where: {id},
+        });
+        return {message: "Projeto deletado com sucesso !!"}
+    }
+
 }
 
-export {CreateProjectService}
+export {ProjectService}
