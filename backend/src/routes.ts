53cca60f8c427d141cb13235e8984aea81c52b20
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2025-11-28 16:49:24 -0300

    feat: criação de rotas

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,17 @@
 
 import { Router } from "express";
+import { projectController } from "./controller/create.project.controller.js";
 
 const router = Router(); 
 
 router.get('/', (req, res) => {
     res.json({ message: "API Metadata Service is running!" });
 });
+
+router.post("/projects", projectController.create);
+router.put("/projects/:id", projectController.update);
+router.get("/projects", projectController.list);
+router.delete("/projects/:id", projectController.delete);
+
 
 export { router };
