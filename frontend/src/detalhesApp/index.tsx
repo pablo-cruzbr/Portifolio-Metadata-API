2e9cffc7a71484855de3828d53899c1909e02f03
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2026-01-21 16:54:36 -0300

    feat: remove buttons

--- frontend/src/detalhesApp/index.tsx
+++ frontend/src/detalhesApp/index.tsx
@@ -1,7 +1,7 @@
 import "../detalhesApp/detalhes.css";
 import { Link } from "react-router-dom";
 
-import { FaGithub, FaPlayCircle, FaAndroid } from "react-icons/fa"; 
+import {FaAndroid} from "react-icons/fa"; 
 import { IoArrowBack } from "react-icons/io5";
 
 function Detalhes() {
@@ -41,24 +41,6 @@ function Detalhes() {
             >
               <FaAndroid size={20}/> Download APK
             </a>
-
-            <Link 
-              to={project.linklivedemo} 
-              className="btn-voltar"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaPlayCircle size={20}/> LiveDemo
-            </Link>
-
-            <Link 
-              to={project.linkgihub} 
-              className="btn-voltar"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaGithub size={20}/> Github
-            </Link>
 
             <Link to="/" className="btn-voltar"> 
               <IoArrowBack size={20}/> Voltar
