28dea09a76bb0ff1956416408d155c61bd194ffb
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2026-02-11 22:51:10 -0300

    feat: new project

--- frontend/src/detalhesApp/index.tsx
+++ frontend/src/detalhesApp/index.tsx
@@ -14,6 +14,14 @@ function Detalhes() {
     linklivedemo: "#", 
     apkPath: "/TechOS-App.apk" 
   };
+  const project2 = {
+    nome: "Meu Apps - Pablo Cruz",
+    descricao: "Mestre das comandas foi desenvolvido com React Native versão 1.0.2.",
+    descricao2: " Aplicativo feito para gerenciamento de pedidos e comandas para restaurantes ou hamburguerias",
+    linkgihub: "https://github.com/seu-usuario",
+    linklivedemo: "#", 
+    apkPath: "/TechOS-App.apk" 
+  };
 
   return (
     <div className="detalhes-container">
@@ -35,6 +43,54 @@ function Detalhes() {
             <br />
             {project.descricao}
             <p>{project.descricao2}</p>
+          </p>
+            <p className="app-title-container"><strong>Android</strong></p>
+          <div className="links">
+          
+          
+          <a 
+            href="https://drive.google.com/uc?export=download&id=1J5WRWivC2cG9eL9r_aMQ0E3PdAcOI9u-" 
+            className="btn-voltar" 
+            style={{ 
+              backgroundColor: '#3DDC84', 
+              color: '#000', 
+              textDecoration: 'none',
+              display: 'inline-flex',
+              alignItems: 'center',
+              padding: '10px 20px',
+              borderRadius: '8px',
+              fontWeight: 'bold',
+              gap: '8px'
+            }}
+          >
+            <FaAndroid size={20}/> Download APK Oficial
+          </a>
+            <Link to="/" className="btn-voltar"> 
+              <IoArrowBack size={20}/> Voltar
+            </Link>
+          </div>
+        </div>
+
+      </div>
+
+      <div className="detalhes-card">
+        <div className="detalhes-info">
+          <h1>{project2.nome}</h1>
+
+            <div className="conteiner-profissional">
+              <div className="box-profissional">
+                  <img className=".img-sobre-mim" src="/tech_os.svg" alt="Imagem do profissional" />
+              </div>
+            </div>
+
+          <p className="app-title-container">
+            <strong> <CiMobile4 size={20}/> Mestre das Comandas - App</strong>
+          </p>
+          <p>
+            <strong>Sobre o Software:</strong>
+            <br />
+            {project2.descricao}
+            <p>{project2.descricao2}</p>
           </p>
             <p className="app-title-container"><strong>Android</strong></p>
           <div className="links">
