bbef63b83ff958576cae50aa490d38abe4b42943
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2026-04-02 16:54:46 -0300

    fix: translation

--- frontendv2/components/Home/Contact/Footer/Footer.tsx
+++ frontendv2/components/Home/Contact/Footer/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="bg-blue-900/20 p-5">
         <h1 className="text-lg text-white/60 text-center">
-            Pablo - Personal Portifolio Website
+           Pablo - Site de Portfólio Pessoal
         </h1>
     </div>
   )
