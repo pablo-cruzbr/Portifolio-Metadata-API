9894ab6b6320e5a9e21b1663f2aa8e92044abc98
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2026-07-02 09:42:13 -0300

    style(services): tint service icons to #00A5E4 via CSS filter

--- frontendv2/components/Home/Service/ServiceCard.tsx
+++ frontendv2/components/Home/Service/ServiceCard.tsx
@@ -10,7 +10,13 @@ type Props = {
 const ServiceCard = ({description, icon, name}: Props) => {
   return (
     <div>
-      <Image src={icon} alt="img" width={60} height={60}/>
+      <Image
+        src={icon}
+        alt="img"
+        width={60}
+        height={60}
+        style={{ filter: "invert(57%) sepia(86%) saturate(600%) hue-rotate(170deg) brightness(101%) contrast(102%)" }}
+      />
       <h1 className="mt-6 text-gray-300">{name}</h1>
       <p className="mt-6 text-gray-300">{description}</p>
     </div>
